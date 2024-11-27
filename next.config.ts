eadb34e61e77650c13fba80dd6fd579b3fe99616
AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>
2024-11-28 01:53:52 +0530

    Updated NextJS to use Page router instead of app router
    Signed-off-by: AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "build"
+  distDir: "build",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
