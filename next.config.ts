eb3b1bdebcdcba7ca53afad373496db582bed687
AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>
2024-11-30 04:34:39 +0530

    Updated to v0.2.5
    Fixed Navbar, added 2nd section (gallery)
    Added footer for all pages
    
    Signed-off-by: AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -8,3 +8,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  images: {
+    remotePatterns: [{
+      'hostname': 'images.unsplash.com',
+    }]
+  }
+}
---
+++ src/components/SliderData.js
@@ -0,0 +1,18 @@
+export const SliderData = [
+    {
+        id: "Slider_Image_1",
+        image: "https://images.unsplash.com/photo-1586694680938-9682c9e1f736?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "Slider_Image_2",
+        image: "https://images.unsplash.com/photo-1567405258710-35a7015252c0?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "Slider_Image_3",
+        image: "https://images.unsplash.com/photo-1578349035260-9f3d4042f1f7?q=80&w=2342&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: "Slider_Image_4",
+        image: "https://images.unsplash.com/photo-1532330393533-443990a51d10?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+];
