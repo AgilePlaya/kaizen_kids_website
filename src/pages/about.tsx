2f8b88945a01a55fc7301b38618b63d735e3c6ca
AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>
2024-11-30 22:11:06 +0530

    Added all pending pages, replaced navbar name with logo
    Bumped version to v0.3.7
    
    Signed-off-by: AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>

---
+++ src/pages/about.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div id="apply" className="min-h-full bg-gray-700">
+      <div className="flex items-center justify-center h-[80vh] bg-fixed bg-center bg-cover custom-img">
+        {/* Overlay */}
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2] h-[80vh] " />
+        <div className="p-10 pr-20 max-w-[1240px] text-white z-[2] mt-[-5rem]">
+          <h2 className="text-5xl font-bold">About Us</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default about;
