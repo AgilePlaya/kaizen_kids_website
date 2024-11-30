bd46f8e2d4d9ae7646e01216580f0f5dd4260cd4
AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>
2024-11-30 14:44:07 +0530

    Added vercel analytics for tracking site usage
    Added provision for opting out of web-tracking
    
    Signed-off-by: AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,14 @@ export default function App({ Component, pageProps }: AppProps) {
       <Navbar />
       <Component {...pageProps} />
       <Footer />
+      <Analytics
+        beforeSend={(event) => {
+          if (localStorage.getItem('va-disable')) {
+            return null;
+          }
+          return event;
+        }}
+      />
     </>
   );
 }
